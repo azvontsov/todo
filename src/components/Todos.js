1c218e26782210a367d5eb5e9e67008e8f6dacf9
Anton <anton@Polinas-MacBook-Air.local>
2022-03-29 23:44:27 -0400

    wip: added Add, Remove, Updete todos

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,9 +1,85 @@
-import React from 'react'
+import React, { useState, useRef} from 'react'
+import { connect } from 'react-redux';
+import { addTodos, removeTodos, updateTodos } from '../redux/reducer';
 
-const Todos = () => {
-  return (
-    <div>Todos</div>
-  )
+const mapStateToProps = (state) => {
+  return {
+    todos: state,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return{
+    addTodo: (obj) => dispatch(addTodos(obj)),
+    removeTodo: (id) => dispatch(removeTodos(id)),
+    updateTodo: (obj) => dispatch(updateTodos(obj)), 
+  }
 }
 
-export default Todos
+const Todos = (props) => {
+  const [todo, setTodo] = useState("");
+
+  const inputRef = useRef(true); 
+
+
+  const changeFocus = () => {
+    inputRef.current.disabled = false;
+    inputRef.current.focus();
+  }
+
+  const update = (id,value,e) => {
+    if(e.which === 13) {
+      // here 13 is key code for enter key
+      props.updateTodo({id, item: value });
+      inputRef.current.disabled = true;
+    }
+  }
+
+
+
+  const handleChange = (e) =>{
+    setTodo(e.target.value); 
+  }
+console.log('props from store', props);
+  return (
+    <div className='addTodos'>
+     <input type="text" 
+     onChange={ (e) => handleChange (e) } 
+     className="todo-input" />
+     <button className="add-btn" onClick={() => props.addTodo({
+      // here is object/todo
+      id: Math.floor(Math.random()*1000),
+      item: todo,
+      completed: false,
+     })} >
+       Add
+     </button>
+     <br />
+
+     <ul>
+       {
+         props.todos.map(item => {
+           return (
+           <li key={item.id}>
+             <textarea 
+             ref={inputRef} 
+             disabled={inputRef} 
+             defaultValue={item.item} 
+             onKeyPress={(e) => update(item.id, inputRef.current.value, e)}
+             />
+              
+             <button onClick={() => changeFocus()} >Edit</button>
+             <button onClick={() => props.removeTodo(item.id)} >
+               Delete
+               </button> {" "}
+           </li>
+           );
+         })}
+     </ul>
+    </div>
+  );
+};
+
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Todos);
